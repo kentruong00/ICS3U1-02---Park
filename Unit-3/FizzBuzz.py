18c02c31648ad4a429123eb2171f817d1e8627fd
kentruong00 <93147737+kentruong00@users.noreply.github.com>
2021-10-25 17:51:30 -0400

    Update FizzBuzz.py

--- Unit-3/FizzBuzz.py
+++ Unit-3/FizzBuzz.py
@@ -1,1 +1,11 @@
-
+i = 1
+while i <= 50:
+    output = ''
+    if i % 3 == 0:
+        output += 'Fizz'
+    if i % 5 == 0:
+        output += 'Buzz'
+    if output == '':
+        output = str(i)
+    i += 1
+    print(output)
