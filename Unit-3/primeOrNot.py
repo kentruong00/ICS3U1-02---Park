98f44f4deb99f5d286176621d1ca72ac89184c86
kentruong00 <93147737+kentruong00@users.noreply.github.com>
2021-10-26 13:03:33 -0400

    Create primeOrNot.py

---
+++ Unit-3/primeOrNot.py
@@ -0,0 +1,18 @@
+int = int(input())
+i = 3
+prime = bool
+if int == 2 or int == 3:
+    prime == True
+elif int % 2 == 0:
+    prime = False 
+else:
+    while i <= int**.5:
+        if int % i == 0:
+            prime = False
+        i+=2
+if prime == False:
+    print('not prime')
+else:
+    print('prime')
+    
+
